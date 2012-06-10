c21caf0626561618aad4e316d6a9b9b759fccbb2
Gabriel Reis and Thais Camilo <narwen+greis+narwen@gmail.com>
2012-06-10 16:03:40 -0400

    Add spec for clicking input type image

--- spec/integration/click_spec.rb
+++ spec/integration/click_spec.rb
@@ -2,45 +2,45 @@ require 'spec_helper'
 
 describe "click action" do
 
-  context "input type submit" do
+  %w(submit image).each do |type|
+    context "input type #{type}" do
 
-    before do
-      driver.navigate.to url_with(body: body)
-      driver.find_element(:css, 'input[type="submit"]').click
-    end
-
-    context "inside a form" do
-      let(:body) do
-        <<-BODY
-        <form method="post" action="/display_params">
-          #{input}
-        </form>
-        BODY
+      before do
+        driver.navigate.to url_with(body: body)
+        driver.find_element(:css, "input[type='#{type}']").click
       end
 
-      let(:input) { %(<input type="submit" name="color" value="red" />) }
+      context "inside a form" do
+        let(:body) do
+          <<-BODY
+            <form method="post" action="/display_params">
+              #{input}
+            </form>
+          BODY
+        end
 
-      it "sends input submit params" do
-        driver.page_source.should include('color=red')
-      end
-    end
+        let(:input) { %(<input type="#{type}" name="color" value="red" />) }
 
-    context "outside a form" do
-      let(:body) do
-        <<-BODY
-        <form method="post" action="/display_params">
-        </form>
-        <input type="submit" name="color" value="red" />
-        BODY
-      end
-
-      it "does not submit the form" do
-        URI(driver.current_url).path.should == "/"
+        it "sends input params" do
+          driver.page_source.should include('color=red')
+        end
       end
 
+      context "outside a form" do
+        let(:body) do
+          <<-BODY
+            <form method="post" action="/display_params"></form>
+            <input type="#{type}" name="color" value="red" />
+          BODY
+        end
+
+        it "does not submit the form" do
+          URI(driver.current_url).path.should == "/"
+        end
+
+      end
     end
   end
-  context "input type image"
   context "button"
   context "anchor"
 end
